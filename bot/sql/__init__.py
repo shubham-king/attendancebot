44c31404e4c401c93412fd95f19a462966515ca6
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-07 11:52:02 +0530

    Update __init__.py

--- bot/sql/__init__.py
+++ bot/sql/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from attendance_bot import DATABASE_URL
+from bot import DATABASE_URL
 
 
 def start() -> scoped_session:
